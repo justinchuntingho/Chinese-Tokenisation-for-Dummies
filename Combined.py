809fe9c008c0e53abcda01b259a8435cbd2ac6d1
Justin Chun-ting Ho <zjustin334@gmail.com>
2017-07-19 16:19:10 +0100

    Combined Jieba, Stanford PKU and Stanford Chinese Tree Bank

--- Combined.py
+++ Combined.py
@@ -1,14 +1,11 @@
-
-# coding: utf-8
-
-# In[6]:
-
 import csv
-
+import os
+import time
+start_time = time.time()
 sourcefile = 'cortesting.txt'
 prefix = 'teststan3'
-pos = 'nvn' #n, v, nvn, all
-tokeniser = 'stanfordpku' # jieba, stanfordpku, stanfordctb
+pos = 'nvn' #'n', 'v', 'nvn', 'all'; only for jieba
+tokeniser = 'jieba' # jieba, stanfordpku, stanfordctb
 jiebadict = 'dict.txt.big'
 jiebauserdict = 'userdict.txt'
 
@@ -21,63 +18,71 @@ def txts(pos):
                     resultwrite.writerow([word.word])
             elif (pos == 'n'):
                 if (len(word.word)>1) and (word.flag.startswith('n')) :#delete the words with only one character.
-                    resultwrite.writerow([word.word])                
+                    resultwrite.writerow([word.word])
             elif (pos == 'v'):
                 if (len(word.word)>1) and (word.flag.startswith('v')) :#delete the words with only one character, export verbs.
-                    resultwrite.writerow([word.word])    
+                    resultwrite.writerow([word.word])
             else:
                 if (len(word.word)>1): #delete the words with only one character, export everything.
                     resultwrite.writerow([word.word])
     resultfile.close()
 
-if (tokeniser == 'jieba'):    
+if (tokeniser == 'jieba'):
     import jieba
     import jieba.posseg as pseg
     jieba.set_dictionary(jiebadict)
-    jieba.load_userdict(jiebauserdict) 
+    jieba.load_userdict(jiebauserdict)
     n=1
     for line in open(sourcefile):
         words = pseg.cut(line)
         txts(pos)
         n=n+1
-        
-if (tokeniser == 'stanfordpku'):
+    print('Done')
+elif (tokeniser == 'stanfordpku'):
     from nltk.tokenize.stanford_segmenter import StanfordSegmenter
     from nltk.tokenize import StanfordTokenizer
+    workingdirectory = os.getcwd()
     segmenter = StanfordSegmenter(
-        path_to_jar=r"/Users/justin/PythonNLP/Stanford/stanford-segmenter.jar",
-        path_to_slf4j=r"/Users/justin/PythonNLP/Stanford/slf4j-api.jar",
-        path_to_sihan_corpora_dict=r"/Users/justin/PythonNLP/Stanford/data",
-        path_to_model=r"/Users/justin/PythonNLP/Stanford/data/pku.gz",
-        path_to_dict=r"/Users/justin/PythonNLP/Stanford/data/dict-chris6.ser.gz"
+        path_to_jar= os.path.join(workingdirectory, 'stanford-segmenter.jar'),
+        path_to_slf4j= os.path.join(workingdirectory, 'slf4j-api.jar'),
+        path_to_sihan_corpora_dict= os.path.join(workingdirectory, 'data'),
+        path_to_model= os.path.join(workingdirectory, 'data', 'pku.gz'),
+        path_to_dict= os.path.join(workingdirectory, 'data', 'dict-chris6.ser.gz')
     )
+    tokenizer = StanfordTokenizer(path_to_jar= os.path.join(workingdirectory, 'stanford-parser.jar'))
     n=1
     for line in open(sourcefile):
         token = segmenter.segment(line)
-        tokenizer = StanfordTokenizer(path_to_jar=r"/Users/justin/PythonNLP/Stanford/stanford-parser.jar")
         words = tokenizer.tokenize(token)
         with open ('%s%s.txt' % (prefix, n), "w", encoding='utf-8') as resultfile:
             resultwrite = csv.writer(resultfile)
             for word in words:
                 resultwrite.writerow([word])
         n=n+1
-
-if (tokeniser == 'stanfordctb'):
-    print('I am still working on it...')   
-        
+    print('Done')
+elif (tokeniser == 'stanfordctb'):
+    from nltk.tokenize.stanford_segmenter import StanfordSegmenter
+    from nltk.tokenize import StanfordTokenizer
+    workingdirectory = os.getcwd()
+    segmenter = StanfordSegmenter(
+        path_to_jar= os.path.join(workingdirectory, 'stanford-segmenter.jar'),
+        path_to_slf4j= os.path.join(workingdirectory, 'slf4j-api.jar'),
+        path_to_sihan_corpora_dict= os.path.join(workingdirectory, 'data'),
+        path_to_model= os.path.join(workingdirectory, 'data', 'ctb.gz'),
+        path_to_dict= os.path.join(workingdirectory, 'data', 'dict-chris6.ser.gz')
+    )
+    tokenizer = StanfordTokenizer(path_to_jar= os.path.join(workingdirectory, 'stanford-parser.jar'))
+    n=1
+    for line in open(sourcefile):
+        token = segmenter.segment(line)
+        words = tokenizer.tokenize(token)
+        with open ('%s%s.txt' % (prefix, n), "w", encoding='utf-8') as resultfile:
+            resultwrite = csv.writer(resultfile)
+            for word in words:
+                resultwrite.writerow([word])
+        n=n+1
+    print('Done')
 else:
     print('Select your tokeniser, Dummy.')
 
-
-        
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
+print("Excution time: %s seconds." % (time.time() - start_time))
